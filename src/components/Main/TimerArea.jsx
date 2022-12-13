a9c036c8f1371767fe60eeb89483645dc4a091df
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-12-13 20:59:36 +0200

    timer logic

--- src/components/Main/TimerArea.jsx
+++ src/components/Main/TimerArea.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import classes from "./TimerArea.module.css";
 
 const TimerArea = () => {
+  const [timer, setTimer] = useState(59);
+  let startTime = Date.now();
+
+  function timePassedInSeconds(startTime) {
+    const timePassed = () => Date.now() - startTime;
+    const convertToSeconds = () => timePassed() / 1000;
+    const countdown = () => 59 - Math.floor(convertToSeconds());
+    if (countdown() < 10 && countdown() > 0) return "0" + countdown();
+    if (countdown() < 0) return "0" + 0;
+    return countdown();
+  }
+
+  useEffect(() => {
+    const progressInterval = setInterval(() => {
+      setTimer(timePassedInSeconds(startTime));
+    }, timePassedInSeconds());
+
+    return () => {
+      clearInterval(progressInterval);
+    };
+  }, []);
+
   return (
     <div className={classes.timerAreaBox}>
-      <p>00:59</p>
+      <p>00:{timer}</p>
     </div>
   );
 };
