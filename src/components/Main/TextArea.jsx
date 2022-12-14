cdd6d5c96bcb05448b82d5dd387aa885218f9ff7
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-12-14 13:55:53 +0200

    onKeyPress start timer

--- src/components/Main/TextArea.jsx
+++ src/components/Main/TextArea.jsx
@@ -1,18 +1,38 @@
 import { useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
 import classes from "./TextArea.module.css";
 
 const TextArea = () => {
   const inputDisabled = useSelector((state) => state.input.inputDisabled);
+  const dispatch = useDispatch();
   const inputRef = useRef(null);
 
   useEffect(() => {
     if (!inputDisabled) inputRef.current.focus();
   }, [inputDisabled]);
 
+  const startTypingHandler = (event) => {
+    const charCode = event.keyCode;
+    if (
+      (charCode > 64 && charCode < 91) ||
+      (charCode > 96 && charCode < 123) ||
+      charCode === 8 ||
+      charCode === 16 ||
+      charCode === 32 ||
+      charCode === 188 ||
+      charCode === 189 ||
+      charCode === 190 ||
+      charCode === 222
+    ) {
+      dispatch(uiActions.startTimer());
+    } else return;
+  };
+
   return (
-    <div className={classes.backgroung}>
+    <div>
       <input
+        onKeyDown={startTypingHandler}
         ref={inputRef}
         disabled={inputDisabled}
         className={classes.textArea}
