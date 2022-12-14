5029a019ff2343a4ebda783c82415cc061194abb
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-12-14 17:48:47 +0200

    added quotes json file

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { shuffleArray } from "../../utils";
+import witcherQuotes from "../../../quotes.json";
+
 import ButtonArea from "./ButtonArea";
 import DataArea from "./DataArea";
 import RestartArea from "./RestartArea";
@@ -6,6 +10,13 @@ import TextArea from "./TextArea";
 import classes from "./Main.module.css";
 
 const Main = () => {
+  const quotes = witcherQuotes.quotes.map(quote => quote.quote);
+
+  const pickNewQuotes = () => {
+    const shuffleQuotes = shuffleArray(quotes)
+    return shuffle.slice(0,10);
+  }
+
   return (
     <section className={classes.main}>
       <ButtonArea />
