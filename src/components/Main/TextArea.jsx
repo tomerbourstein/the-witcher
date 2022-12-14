29dd6c3fed466f200cc455eea8a4627a89a5d426
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-12-14 16:15:00 +0200

    reset input value on click restart button

--- src/components/Main/TextArea.jsx
+++ src/components/Main/TextArea.jsx
@@ -1,19 +1,28 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { uiActions } from "../../store/ui-slice";
 import classes from "./TextArea.module.css";
 
-const TextArea = () => {
+const TextArea = (props) => {
   const inputDisabled = useSelector((state) => state.input.inputDisabled);
   const dispatch = useDispatch();
+  const [focused, setFocused] = useState(true);
   const inputRef = useRef(null);
 
   useEffect(() => {
-    if (!inputDisabled) inputRef.current.focus();
-  }, [inputDisabled]);
+    if (!inputDisabled) inputRef.current.value = null;
+    if (!inputDisabled || !focused) {
+      inputRef.current.focus();
+      setFocused(true);
+    }
+  }, [inputDisabled, focused]);
 
+  const inputBluerHandler = () => {
+    setFocused(false);
+  };
   const startTypingHandler = (event) => {
     const charCode = event.keyCode;
+    if (charCode === 9 || charCode === 18) event.preventDefault();
     if (
       (charCode > 64 && charCode < 91) ||
       (charCode > 96 && charCode < 123) ||
@@ -32,13 +41,14 @@ const TextArea = () => {
   return (
     <div>
       <input
-        onKeyDown={startTypingHandler}
+        type="text"
+        placeholder="Evil is Evil. Lesser, greater, middling… Makes no difference. The degree is arbitary. The defi..."
+        className={classes.textArea}
         ref={inputRef}
         disabled={inputDisabled}
-        className={classes.textArea}
-        type="text"
+        onKeyDown={startTypingHandler}
+        onBlur={inputBluerHandler}
         autoFocus
-        placeholder="Evil is Evil. Lesser, greater, middling… Makes no difference. The degree is arbitary. The defi..."
       ></input>
     </div>
   );
