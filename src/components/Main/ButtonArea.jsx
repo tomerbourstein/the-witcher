774c8421fd5812aae8d316a7e254ea49f5a005cb
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-12-17 10:46:04 +0200

    added geralt picture

--- src/components/Main/ButtonArea.jsx
+++ src/components/Main/ButtonArea.jsx
@@ -3,10 +3,21 @@ import { uiActions } from "../../store/ui-slice";
 import Button from "../UI/Button";
 import TimerArea from "./TimerArea";
 
+import geralt0 from "../../assets/geralt0.png";
+import geralt1 from "../../assets/geralt1.png";
+import geralt2 from "../../assets/geralt2.png";
+import geralt3 from "../../assets/geralt3.png";
+import geralt4 from "../../assets/geralt4.png";
+import geralt5 from "../../assets/geralt5.png";
+import geraltBad2 from "../../assets/geralt-bad2.png";
+import geraltBad3 from "../../assets/geralt-bad3.png";
+import geraltBad4 from "../../assets/geralt-bad4.png";
+
 import classes from "./ButtonArea.module.css";
 
 const ButtonArea = () => {
   const timerRender = useSelector((state) => state.ui.timerRender);
+  const typedChar = useSelector((state) => state.data.typedChar);
   const dispatch = useDispatch();
 
   const initialTimer = (
@@ -14,13 +25,45 @@ const ButtonArea = () => {
       <p>01:00</p>
     </div>
   );
-
+  console.log(typedChar);
+  let geraltFigure;
+  switch (true) {
+    case typedChar >= 1 && typedChar <= 60:
+      geraltFigure = geraltBad2;
+      break;
+    case typedChar >= 60 && typedChar <= 100:
+      geraltFigure = geraltBad3;
+      break;
+    case typedChar >= 100 && typedChar <= 130:
+      geraltFigure = geraltBad4;
+      break;
+    case typedChar >= 130 && typedChar <= 160:
+      geraltFigure = geralt1;
+      break;
+    case typedChar >= 160 && typedChar <= 190:
+      geraltFigure = geralt3;
+      break;
+    case typedChar >= 190 && typedChar <= 205:
+      geraltFigure = geralt2;
+      break;
+    case typedChar >= 205 && typedChar <= 220:
+      geraltFigure = geralt4;
+      break;
+    case typedChar > 220:
+      geraltFigure = geralt5;
+      break;
+    default:
+      geraltFigure = geralt0;
+  }
   const startTypingHandler = (event) => {
     console.log(event.type);
     dispatch(uiActions.startTimer());
   };
   return (
     <div className={classes.buttonAreaBox}>
+      <div className={classes.wrapper}>
+        <img className={classes.geralt} src={geraltFigure} />
+      </div>
       <Button content={"START TYPING"} onClick={startTypingHandler} />
       {timerRender ? <TimerArea /> : initialTimer}
     </div>
--- src/components/Main/DataArea.jsx
+++ src/components/Main/DataArea.jsx
@@ -1,15 +1,16 @@
 import { useSelector } from "react-redux";
 import classes from "./DataArea.module.css";
+
 const DataArea = () => {
   const typedChar = useSelector((state) => state.data.typedChar);
 
   return (
     <div className={classes.dataArea}>
       <div className={classes.counterBox}>
-        YOU TYPED <span className={classes.typedChar}>{typedChar}</span> LETTERS
+        YOU TYPED <span className={classes.typedChar}>{typedChar}</span> LETTERS 
       </div>
       <div className={classes.quote}>
-        "This world doesn't need a hero. It needs a professional"
+        "This World Doesn't Need a Hero. It Needs a Professional" 
       </div>
       <div className={classes.quote}>Geralt of Rivia</div>
     </div>
