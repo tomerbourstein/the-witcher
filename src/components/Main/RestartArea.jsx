c81ce39ca1b1d3dc892a7fdd6f340ac289e902ed
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-12-19 14:42:48 +0200

    reset button bug fix

--- src/components/Main/RestartArea.jsx
+++ src/components/Main/RestartArea.jsx
@@ -21,8 +21,9 @@ const RestartArea = () => {
     dispatch(dataActions.resetData());
   };
   return (
-    <div onClick={restartTypingHandler}>
+    <div>
       <svg
+        onClick={restartTypingHandler}
         className={`${classes.restartIcon} ${animation}`}
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
